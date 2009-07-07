9020f8b0f0dbb308a19736e928f7d15998cb643c
Rich Cavanaugh <rich.cavanaugh@tycoonresearch.com>
2009-07-07 16:45:25 -0400

    less inflamatory

--- examples/nine-b.rb
+++ examples/nine-b.rb
@@ -1,13 +1,18 @@
 module PrependRich
-  def prepend_rich; "Rich #{self}"; end
+  def prepend_rich
+    "Rich #{self}"
+  end
 end
 
 str = "says 'Hello' from a module."
 
 puts str.prepend_rich           # => NoMethodError
 
-class << str; include PrependRich; end
+# Evil
+class << str
+  include PrependRich
+end
 
 puts str.prepend_rich           # => Rich says 'Hello' from a module.
 
-puts "hates PHP.".prepend_rich  # => NoMethodError
+puts "prefers Ruby.".prepend_rich  # => NoMethodError
